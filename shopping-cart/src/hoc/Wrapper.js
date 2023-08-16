9e3520e156da3aa0dad7ea4d4dab5e60e640381d
0xnightcat_ <141747393+0xNightcat@users.noreply.github.com>
2023-08-16 18:14:55 +0330

    Delete shopping-cart directory

--- shopping-cart/src/hoc/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import './Wrapper.css';
-
-const Wrapper = (props) => {
-   return (
-      <div className={props.class}>{props.children}</div>
-   )
-}
-
-export default Wrapper;
--- shopping-cart/src/index.js
+++
@@ -1,7 +0,0 @@
-import reactDom from 'react-dom';
-
-import './index.css';
-
-import App from './App';
-
-reactDom.render(<App />, document.querySelector('#root'));
